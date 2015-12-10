0b64fbe6dd069abc9e1e69f0e99455ec291ee107
mithrilwoodrat <mithrilwoodrat@gmail.com>
2015-12-10 14:27:37 +0800

    add web-config hook

--- .emacs.d/config/web-config.el
+++ .emacs.d/config/web-config.el
@@ -28,8 +28,11 @@
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-markup-indent-offset 4) ; html element
+  (setq web-mode-css-indent-offset 4)
+  (setq web-mode-code-indent-offset 4)
   (setq tab-width 4)
+  (setq indent-tabs-mode nil)
 )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
 
