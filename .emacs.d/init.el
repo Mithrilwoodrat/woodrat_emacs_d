2730ccd6db0adc82fb5aa2c483ed276fef4e5c3a
Mithrilwoodrat <mithrilwoodrat@gmail.com>
2017-10-20 15:43:50 +0800

    change chinese-pyim to pyim

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -95,10 +95,10 @@
  )
 
 ;; input method
-(require 'chinese-pyim)
-(require 'chinese-pyim-basedict) ; 拼音词库设置，五笔用户 *不需要* 此行设置
-(chinese-pyim-basedict-enable)   ; 拼音词库，五笔用户 *不需要* 此行设置
-(setq default-input-method "chinese-pyim")
+(require 'pyim)
+(require 'pyim-basedict) ; 拼音词库设置，五笔用户 *不需要* 此行设置
+(pyim-basedict-enable)   ; 拼音词库，五笔用户 *不需要* 此行设置
+(setq default-input-method "pyim")
 (global-set-key (kbd "C-=") 'toggle-input-method)
 
 ;;
